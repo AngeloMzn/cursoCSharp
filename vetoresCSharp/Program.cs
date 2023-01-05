ee618b714638879661b9f4355751925bb9277dd2
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-05 15:25:29 -0300

    Add files via upload

---
+++ vetoresCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace vetoresCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("digite o tamanho do vetor: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine(vet[i].ToString("F2"));
+
+            }
+           
+
+
+
+
+        }
+    }
+}
