95298bb2fbe8bd5ad61b7046bee6157983f14199
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-05 15:24:57 -0300

    Add files via upload

---
+++ tipagemCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 32;
+            salario = 4000.9;
+            altura = 1.72;
+            genero = 'F';
+            nome = "Maria Silva";
+
+            System.Console.WriteLine(idade); // usar cw tab pra pegar comando rapido
+            System.Console.WriteLine(salario);
+            System.Console.WriteLine(genero);
+            System.Console.WriteLine(nome);        
+        }
+    }
+}
