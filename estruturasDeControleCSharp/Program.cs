c4cc6a1098579cd2686a392a375f46245a4afff8
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-05 15:22:47 -0300

    Add files via upload

---
+++ estruturasDeControleCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace estruturasDeControleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b, a;
+            a = 5;
+
+            if(a >= 5)
+            {
+                b = 10;
+            }
+            else
+            {
+                b = 6;
+            }
+            System.Console.WriteLine(b);
+
+            while(a != 0)
+            {
+                a--;
+                System.Console.WriteLine(a);
+            }
+
+            for(int i = 0; i < b; i++ )
+            {
+                System.Console.WriteLine(i);
+            }
+
+        }
+    }
+}
